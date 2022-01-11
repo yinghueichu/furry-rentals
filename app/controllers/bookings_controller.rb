8eb27269a0f1807649051e3fffe62297790a6a25
yinghueichu <yinghuei.chu@essec.edu>
2022-01-11 20:41:32 +0100

    add bookings controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,26 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @animal = Animal.find(params[:animal_id])
+    @booking = Booking.new(booking_params)
+    @booking.animal = @animal
+    if @booking.save
+      redirect_to animal_path(@animal)
+    else
+      render 'animals/show'
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :animal
   belongs_to :user
+  validates :start_date, :animal, presence: true
 end
