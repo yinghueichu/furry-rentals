cf86fc00b4f25a8bf15eedde925ae400a1adc78b
yinghueichu <yinghuei.chu@essec.edu>
2022-01-13 20:15:53 +0100

    add edit page

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,12 +1,12 @@
 class AnimalsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :index, :root]
+  before_action :set_animal, only: [:show, :edit, :update]
 
   def index
     @animals = Animal.all
   end
 
   def show
-    @animal = Animal.find(params[:id])
     @booking = Booking.new
   end
 
@@ -24,7 +24,18 @@ class AnimalsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @animal.update(params[:animal])
+  end
+
   private
+
+  def set_animal
+    @animal = Animal.find(params[:id])
+  end
 
   def strong_params
     params.require(:animal).permit(:name, :species, :address)
