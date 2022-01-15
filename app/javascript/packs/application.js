9ae49fdcd3e8863dd6778a25afa9525177aad1e7
manon rodriguez mate <ma.rodriguez.mate@gmail.com>
2022-01-15 13:43:12 +0100

    autocomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,11 +28,13 @@ import "stimulus-flatpickr"
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+  initAutocomplete();
 });
 
 import { Application } from 'stimulus'
