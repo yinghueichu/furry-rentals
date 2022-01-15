124cf2c715410520517759219c71b161419b84a0
yinghueichu <yinghuei.chu@essec.edu>
2022-01-15 11:36:54 +0100

    add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'animals#index'
-  resources :animals, only: [:create, :new, :edit, :update]
-  resources :animals, only: [:index, :show] do
+
+  resources :users, only: [:show, :update] do
+    member do
+      resources :bookings, only: [:index]
+    end
+  end
+
+  resources :animals, only: []
+  resources :animals, only: [:index, :show, :create, :new, :edit, :update] do
     resources :bookings, only: [:create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
