411a455425a1b968169f7ade319b6a2a86deab19
manon rodriguez mate <ma.rodriguez.mate@gmail.com>
2022-01-15 12:39:11 +0100

    fit map to markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,6 +1,12 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 3 });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -16,6 +22,7 @@ const initMapbox = () => {
         .setLngLat([marker.lng, marker.lat])
         .addTo(map);
     });
+    fitMapToMarkers(map, markers);
   }
 };
 
