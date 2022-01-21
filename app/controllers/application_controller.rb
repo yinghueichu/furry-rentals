dec09a27799ab03df4382e1e5c49e732ed8dc9b1
yinghueichu <yinghuei.chu@essec.edu>
2022-01-21 22:05:51 +0100

    save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
+  end
+
 end
